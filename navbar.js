325af130f546340de1861b07618fbcd3b9ce327b
shafiqmh <mohammed.shafiq@academytrainee.com>
2019-09-09 17:32:57 +0100

    front end initial

---
+++ navbar.js
@@ -0,0 +1,12 @@
+
+function handleClickIndex() {
+    location.href = 'index.html';
+}
+
+function handleClickTeams() {
+    location.href = 'teams.html';
+}
+
+function handleClickLeagues() {
+    location.href = 'leagues.html';
+}
