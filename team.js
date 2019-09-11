cf5b40d715d508074945f1dcc2c9628258071f02
shafiqmh <mohammed.shafiq@academytrainee.com>
2019-09-11 13:57:32 +0100

    created team and league tables that obtain info from postman

--- team.js
+++ team.js
@@ -1,18 +1,57 @@
-function makeRequest(requestType, url, whatToSend) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.onload = () => {
-            if (xhr.status == 200) {
-                resolve(xhr.response);
-            } else {
-                const reason = new Error("Rejected");
-                reject(reason);
+// function makeRequest(requestType, url, whatToSend) {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.onload = () => {
+//             if (xhr.status == 200) {
+//                 const data = JSON.parse(xhr.response);
+//                 resolve(xhr.response);
+//             } else {
+//                 const reason = new Error("Rejected");
+//                 reject(reason);
+//             }
+//         };
+//         xhr.open("GET", teamURL);
+//         xhr.send();
+//     });
+// console.log(data);
+// console.log("hidisjd");
+// }
+
+makeRequest("GET", teamURL)
+    .then(data => {
+        console.log(data);
+
+        let teamKeys = Object.keys(data[0]);
+
+        function generateTableHead(table, data) {
+            let thead = table.createTHead();
+            let row = thead.insertRow();
+            for (let key of teamKeys) {
+                let th = document.createElement("th");
+                let text = document.createTextNode(key);
+                th.appendChild(text);
+                row.appendChild(th);
             }
-        };
-        xhr.open(requestType, url);
-        xhr.send(whatToSend);
+        }
+
+        function generateTable(table, data) {
+            for (let element of data) {
+                let row = table.insertRow();
+                for (let key in element) {
+                    let cell = row.insertCell();
+                    let text = document.createTextNode(element[key]);
+                    cell.appendChild(text);
+                }
+            }
+        }
+
+        let table = document.getElementById("teamsTable");
+        generateTableHead(table, data);
+        generateTable(table, data)
+    })
+    .catch(error => {
+        console.log(error);
     });
-}
 
 // function removeAllChildren(id) {
 //     let result = document.getElementById(id);
@@ -22,55 +61,55 @@ function makeRequest(requestType, url, whatToSend) {
 
 // }
 
-function addTeam(newEntry, aRow) {                   //addToTable
-    let iTeamID = document.createElement('td');
-    iTeamID.innerHTML = newEntry.teamID;
-    let iTeamName = document.createElement('td');
-    iTeamName.innerHTML = newEntry.teamName;
-    let iTeamLocation = document.createElement('td');
-    iTeamLocation.innerHTML = newEntry.teamLocation;
-    let iTeamStadium = document.createElement('td');
-    iTeamStadium.innerHTML = newEntry.teamStadium;
-    let deleteButton = document.createElement('td');
-    deleteButton.innerHTML = `<button type="button" class="btn btn-secondary" onclick='destroy(${newEntry.teamID})' > Delete</button >`;
-    let readOneButton = document.createElement('td');
-    readOneButton.innerHTML = `<button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal" onclick ='readOne(${newEntry.teamID})' > More Details </button >`;
+// function addTeam(newEntry, aRow) {                   //addToTable
+//     let iTeamID = document.createElement('td');
+//     iTeamID.innerHTML = newEntry.teamID;
+//     let iTeamName = document.createElement('td');
+//     iTeamName.innerHTML = newEntry.teamName;
+//     let iTeamLocation = document.createElement('td');
+//     iTeamLocation.innerHTML = newEntry.teamLocation;
+//     let iTeamStadium = document.createElement('td');
+//     iTeamStadium.innerHTML = newEntry.teamStadium;
+//     let deleteButton = document.createElement('td');
+//     deleteButton.innerHTML = `<button type="button" class="btn btn-secondary" onclick='destroy(${newEntry.teamID})' > Delete</button >`;
+//     let readOneButton = document.createElement('td');
+//     readOneButton.innerHTML = `<button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal" onclick ='readOne(${newEntry.teamID})' > More Details </button >`;
 
-    aRow.appendChild(iTeamID);
-    aRow.appendChild(iTeamName);
-    aRow.appendChild(iTeamLocation);
-    aRow.appendChild(iTeamStadium);
-    aRow.appendChild(deleteButton);
-    aRow.appendChild(readOneButton);
-}
+//     aRow.appendChild(iTeamID);
+//     aRow.appendChild(iTeamName);
+//     aRow.appendChild(iTeamLocation);
+//     aRow.appendChild(iTeamStadium);
+//     aRow.appendChild(deleteButton);
+//     aRow.appendChild(readOneButton);
+// }
 
 
-function addTeam() {
-  const xhr = new XMLHttpRequest();
-  let name = document.getElementById('teamName').value;
-  let location = document.getElementById('teamLocation').value;
-  let stadium = document.getElementById('teamStadium').value;
+// function addTeam() {
+//   const xhr = new XMLHttpRequest();
+//   let name = document.getElementById('teamName').value;
+//   let location = document.getElementById('teamLocation').value;
+//   let stadium = document.getElementById('teamStadium').value;
 
-  let json = JSON.stringify({
-    "name": `${name}`,
-    "location": `${location}`,
-    "stadium": `${stadium}`,
-  });
+//   let json = JSON.stringify({
+//     "name": `${name}`,
+//     "location": `${location}`,
+//     "stadium": `${stadium}`,
+//   });
 
-  console.log(json);
-  xhr.open('POST', 'http://localhost:9966/petclinic/api/owners', true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = () => {
-    console.log(xhr.response);
-    if (xhr.status == 201) {
-      getOwners();
-    } else {
-      document.getElementById('Error').innerHTML = 'Oops... something went wrong!'
-    }
-  }
-  getOwners();
-  xhr.send(json);
-  // return false;
-}
+//   console.log(json);
+//   xhr.open('POST', 'http://localhost:9966/petclinic/api/owners', true);
+//   xhr.setRequestHeader('Content-Type', 'application/json');
+//   xhr.onload = () => {
+//     console.log(xhr.response);
+//     if (xhr.status == 201) {
+//       getOwners();
+//     } else {
+//       document.getElementById('Error').innerHTML = 'Oops... something went wrong!'
+//     }
+//   }
+//   getOwners();
+//   xhr.send(json);
+//   // return false;
+// }
 
 
